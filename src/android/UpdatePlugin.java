42386bb62afb7527fe7b656be19f88bc4cd3def8
MrSpark2591 <mr.spark2591@gmail.com>
2020-01-16 14:40:47 +0530

    changes

--- src/android/UpdatePlugin.java
+++ src/android/UpdatePlugin.java
@@ -23,90 +23,90 @@ import org.json.JSONArray;
 import static java.lang.System.out;
 
 public class UpdatePlugin extends CordovaPlugin {
-  public int REQUEST_CODE = 7;
-  @Override 
-  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-    super.initialize(cordova, webView);
+ public int REQUEST_CODE = 7;
+ @Override
+ public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+  super.initialize(cordova, webView);
+ }
+
+ @Override
+ public boolean execute(String action, JSONArray args,
+  final CallbackContext callbackContext) {
+  // Verify that the user sent a 'show' action
+  if (!action.equals("show")) {
+   callbackContext.error("\"" + action + "\" is not a recognized action.");
+   return false;
   }
+  Context context = this.cordova.getActivity().getApplicationContext();
 
-  @Override
-  public boolean execute(String action, JSONArray args,
-    final CallbackContext callbackContext) {
-      // Verify that the user sent a 'show' action
-      if (!action.equals("show")) {
-        callbackContext.error("\"" + action + "\" is not a recognized action.");
-        return false;
-      }
-      Context context = this.cordova.getActivity().getApplicationContext();
+  AppUpdateManager appUpdateManager = AppUpdateManagerFactory.create(context);
 
-      AppUpdateManager appUpdateManager = AppUpdateManagerFactory.create(context);
-
-      Task<AppUpdateInfo> appUpdateInfoTask = appUpdateManager.getAppUpdateInfo();
-      try {
-      appUpdateInfoTask.addOnSuccessListener(appUpdateInfo -> {
-        System.out.println(appUpdateInfo);  
-        if (appUpdateInfo.updateAvailability()
-                == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS) {
-                // If an in-app update is already running, resume the update.
-          try {
-                appUpdateManager.startUpdateFlowForResult(
-                    appUpdateInfo,
-                    AppUpdateType.IMMEDIATE,
-                    cordova.getActivity(),
-                    REQUEST_CODE);
-              } catch(Exception e){
-              e.printStackTrace();
-              }
-        }
-        
-        if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
-          && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) {
-          try {
-              appUpdateManager.startUpdateFlowForResult(
-                appUpdateInfo,
-                AppUpdateType.IMMEDIATE,
-                cordova.getActivity(),
-                 REQUEST_CODE
-              );
-            } catch(Exception e){
-              e.printStackTrace();
-            }
-        }
-
-        PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, appUpdateInfo.toString());
-        callbackContext.sendPluginResult(pluginResult);
-      });
-      } catch(Exception e){
-          e.printStackTrace();
-      }
-      return true;
-  }
-
-  @Override
-  public void onResume(boolean multitasking) {
-    Context context = this.cordova.getActivity().getApplicationContext();
-
-    AppUpdateManager appUpdateManager = AppUpdateManagerFactory.create(context);
-
-    Task<AppUpdateInfo> appUpdateInfoTask = appUpdateManager.getAppUpdateInfo();
-   appUpdateManager
-      .getAppUpdateInfo()
-      .addOnSuccessListener(
-          appUpdateInfo -> {
-            if (appUpdateInfo.updateAvailability()
-                == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS) {
-                // If an in-app update is already running, resume the update.
-               try {
-                appUpdateManager.startUpdateFlowForResult(
-                    appUpdateInfo,
-                    AppUpdateType.IMMEDIATE,
-                    cordova.getActivity(),
-                    REQUEST_CODE);
-                 } catch(Exception e){
-                  e.printStackTrace();
-                }
-            }
-          });
+  Task < AppUpdateInfo > appUpdateInfoTask = appUpdateManager.getAppUpdateInfo();
+  try {
+   appUpdateInfoTask.addOnSuccessListener(appUpdateInfo -> {
+    System.out.println(appUpdateInfo);
+    if (appUpdateInfo.updateAvailability() ==
+     UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS) {
+     // If an in-app update is already running, resume the update.
+     try {
+      appUpdateManager.startUpdateFlowForResult(
+       appUpdateInfo,
+       AppUpdateType.IMMEDIATE,
+       cordova.getActivity(),
+       REQUEST_CODE);
+     } catch (Exception e) {
+      e.printStackTrace();
+     }
     }
+
+    if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE &&
+     appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) {
+     try {
+      appUpdateManager.startUpdateFlowForResult(
+       appUpdateInfo,
+       AppUpdateType.IMMEDIATE,
+       cordova.getActivity(),
+       REQUEST_CODE
+      );
+     } catch (Exception e) {
+      e.printStackTrace();
+     }
+    }
+
+    PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, appUpdateInfo.toString());
+    callbackContext.sendPluginResult(pluginResult);
+   });
+  } catch (Exception e) {
+   e.printStackTrace();
+  }
+  return true;
+ }
+
+ @Override
+ public void onResume(boolean multitasking) {
+  Context context = this.cordova.getActivity().getApplicationContext();
+
+  AppUpdateManager appUpdateManager = AppUpdateManagerFactory.create(context);
+
+  Task < AppUpdateInfo > appUpdateInfoTask = appUpdateManager.getAppUpdateInfo();
+  appUpdateManager
+   .getAppUpdateInfo()
+   .addOnSuccessListener(
+    appUpdateInfo -> {
+     if (appUpdateInfo.updateAvailability() ==
+      UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS) {
+      // If an in-app update is already running, resume the update.
+      try {
+       appUpdateManager.startUpdateFlowForResult(
+        appUpdateInfo,
+        AppUpdateType.IMMEDIATE,
+        cordova.getActivity(),
+        REQUEST_CODE);
+      } catch (Exception e) {
+       e.printStackTrace();
+      }
+     }
+    });
+ }
 
 }
